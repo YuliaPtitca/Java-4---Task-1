4aab38643a41b4c4ba8cc596d2ac08ea35560b08
unknown <qa.yulia.ptitca@yandex.ru>
2022-07-15 11:13:29 +0300

    Create new test for unregistered

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -17,12 +17,43 @@ public class BonusServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndOverLimit() {
+    void shouldCalculateRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
 
         long amount = 1_000_000;
         boolean registered = true;
+        long expected = 500;
+
+
+        long actual = service.calculate(amount, registered);
+
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+
+        long actual = service.calculate(amount, registered);
+
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+
+        long amount = 1_000_000;
+        boolean registered = false;
         long expected = 500;
 
 
